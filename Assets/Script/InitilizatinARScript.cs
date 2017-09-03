a4b9d50de1079d5d11c19545a722daec426f04a5
lazyZhou1997 <LazyZhou1997@gmail.com>
2017-09-03 17:03:56 +0800

    完成初步开发

---
+++ Assets/Script/InitilizatinARScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitilizatinARScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //自动对焦
+        if (!Vuforia.CameraDevice.Instance.SetFocusMode(Vuforia.CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO)) {
+            Debug.Log("相机自动对焦不可用");
+        }
+        
+    }
+}
---
+++ Assets/Script/LoadSceneScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneScript : MonoBehaviour {
+
+    //加载下一个场景
+    public void LoadScene(string sceneName){
+        //加载下一个场景
+        SceneManager.LoadScene(sceneName);
+    }
+}
