40b14fbd8339c061029106f82916d22fc1d23206
lazyZhou1997 <LazyZhou1997@gmail.com>
2017-11-07 21:28:21 +0800

    2017/11/7 初步调试结束

--- Assets/Script/InitilizatinARScript.cs
+++ Assets/Script/InitilizatinARScript.cs
@@ -1,23 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Vuforia;
 
 public class InitilizatinARScript : MonoBehaviour
 {
 
+	void Start ()
+	{
+		var vuforia = VuforiaARController.Instance;
+		vuforia.RegisterVuforiaStartedCallback(OnVuforiaStarted);
+		vuforia.RegisterOnPauseCallback(OnPaused);
+	}
 
-	// Use this for initialization
-	void Start () {
-        //自动对焦
-        if (!Vuforia.CameraDevice.Instance.SetFocusMode(Vuforia.CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO))
-        {
-            Debug.Log("相机自动对焦不可用");
-        }
-    }
-	
-	// Update is called once per frame
-	void Update () {
+	private void OnVuforiaStarted()
+	{
+		CameraDevice.Instance.SetFocusMode(
+			CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+	}
 
-        
-    }
+	private void OnPaused(bool paused)
+	{
+		if (!paused) // resumed
+		{
+			// Set again autofocus mode when app is resumed
+			CameraDevice.Instance.SetFocusMode(
+				CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+		}
+	}
 }
