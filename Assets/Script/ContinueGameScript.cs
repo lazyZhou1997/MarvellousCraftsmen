f5e9bfcccbbb6275cb9dd4f19c3050d40fb92c43
lazyZhou1997 <LazyZhou1997@gmail.com>
2017-11-09 23:53:00 +0800

    2017/11/9 完成存档初步功能

--- Assets/Script/ContinueGameScript.cs
+++ Assets/Script/ContinueGameScript.cs
@@ -20,6 +20,18 @@ public class ContinueGameScript : MonoBehaviour
 	public void LoadArchieveScene()
 	{
 		//获取当前存档中的数据
+		string gameDataJson = PlayerPrefs.GetString(ConfigureClass.archiveName);
+		GameData gameData = JsonUtility.FromJson<GameData>(gameDataJson);
+		//判断是不是有存档数据
+		if (null==gameData)
+		{
+			Debug.Log("存档不存在");
+			return;
+		}
 
+		//设置读取存档标志
+		ConfigureClass.isreadGameData = true;
+		//跳转到存档所在场景
+		SplashLoad.StartSplash(gameData.sceneName,2);
 	}
 }
