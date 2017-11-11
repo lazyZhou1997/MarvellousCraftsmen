9c00297c83fc5d5eb6edfbd4a13b1fcb6ed3a2c5
lazyZhou1997 <LazyZhou1997@gmail.com>
2017-11-12 00:33:13 +0800

    2017/11/12 realise版本发布

---
+++ Assets/Script/PlayVedioScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayVedioScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//播放视频
+		Handheld.PlayFullScreenMovie("gameintroduction.mp4", Color.black, FullScreenMovieControlMode.Full);
+	}
+
+}
